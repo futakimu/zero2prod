7e2598c99cee55814fa4294856732ef8d4d83957
futakimu <futakimu@gmail.com>
2021-07-26 19:42:18 +0900

    Add db connection setting

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
+use sqlx::{PgConnection, Connection};
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 
 #[actix_rt::test]
 async fn health_check_works() {
@@ -32,6 +34,11 @@ fn spawn_app() -> String{
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
