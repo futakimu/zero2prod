58dea9a7c270cd74538e1a7a9d2d6a0bfe1c1009
futakimu <futakimu@gmail.com>
2021-07-31 13:43:25 +0900

    Add Logs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,18 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!(
+        "Saving new subscriber details in the database",
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -26,9 +38,11 @@ pub async fn subscribe(
     .execute(pool.as_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!( "Failed to execute query: {:?}", e );
             HttpResponse::InternalServerError().finish()
         }
     }
