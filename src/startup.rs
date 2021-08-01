bad2faf1cba980533008e8b44ac19abf51baec09
futakimu <futakimu@gmail.com>
2021-08-01 09:43:26 +0900

    Add TracingLogger

--- src/startup.rs
+++ src/startup.rs
@@ -3,7 +3,7 @@ use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 use sqlx::PgPool;
-use actix_web::middleware::Logger;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(
     listener: TcpListener,
@@ -12,7 +12,7 @@ pub fn run(
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
